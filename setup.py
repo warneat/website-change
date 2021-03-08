292c5259b73d661a157700d3e580548e75c7f793
warneat <41758828+warneat@users.noreply.github.com>
2021-03-08 14:23:46 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='website_change',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/warneat/website_change',
+    license='MIT',
+    author='Dominik Kimmerle',
+    author_email='kontakt@dominikkimmerle.de',
+    description='Get notified, when website changes.'
+)
